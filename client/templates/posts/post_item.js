2ac3c0293bb3afc9999e8890357e2a76b78a0f0c
youngmin kim <youngmin@youngminui-MacBook-Pro.local>
2015-06-11 20:32:25 +0900

    post item added

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,3 +1,4 @@
+
 Template.postItem.helpers({
   ownPost: function() {
     return this.userId == Meteor.userId();
@@ -21,5 +22,34 @@ Template.postItem.events({
   'click .upvotable': function(e) {
     e.preventDefault();
     Meteor.call('upvote', this._id);
+  },
+  'click .item': function(e) {
+    e.preventDefault();
+    $(e.target).siblings(".active").removeClass("active");
+    $(e.target).addClass('active');
   }
 });
+
+Template.postItem.rendered = function() {
+
+  // if (!this.rendered){
+    var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    mapOption = { 
+        center: new daum.maps.LatLng(this.data.location.latitude, this.data.location.longitude), // 지도의 중심좌표
+        // center: new daum.maps.LatLng("37.69600304694161", "128.8919231052503"),
+        level: 3 // 지도의 확대 레벨
+    };
+
+    // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+    var map = new daum.maps.Map(mapContainer, mapOption); 
+
+    // 지도를 클릭한 위치에 표출할 마커입니다
+    var marker = new daum.maps.Marker({ 
+        // 지도 중심좌표에 마커를 생성합니다 
+        position: map.getCenter() 
+    }); 
+    // 지도에 마커를 표시합니다
+    marker.setMap(map);
+    // this.rendered = true;
+  // }
+}
