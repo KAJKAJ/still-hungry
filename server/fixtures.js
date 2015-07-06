304cb7e91e91db5d3fe2fb3e410ebf0c50862800
youngmin kim <youngmin@youngminui-MacBook-Pro.local>
2015-07-06 17:02:25 +0900

    feature 9 in progress

--- server/fixtures.js
+++ server/fixtures.js
@@ -4,15 +4,22 @@ if (Posts.find().count() === 0) {
   var rawCSV = Assets.getText('fixture/sheet1.csv');
   var rowsParsed = Baby.parse(rawCSV);
   var rows = rowsParsed.data;
+  var restaurants = Array();
 
-    var adminId = Meteor.users.insert({
+  var adminId = Meteor.users.insert({
     profile: {name: 'youngmin'}
   });
 
   var admin = Meteor.users.findOne(adminId);
 
   for(var i=1; i < rows.length; i++ ){
+
     var element = rows[i];
+
+    if(element[0] !== 'S02E13') continue;
+
+    // title is empty
+    if(element[2] == '') continue;
 
     var restaurant = {
       imageUrl: '',
@@ -39,25 +46,29 @@ if (Posts.find().count() === 0) {
       likeusers: [], likes:0
     };
 
+    // fetch value added information from daum API
+    // 
     var requestURI = 'https://apis.daum.net/local/v1/search/keyword.json';
     var query = restaurant.tel? restaurant.tel 
                     : (restaurant.location.city + ' ' + restaurant.location.division + ' ' + restaurant.location.address);
-    // HTTP.call('GET', requestURI, 
-    //     {params: { apikey: 'f836162338ea24d0e221975199009a2d', query : query } }
-    //     , function(error, result) {
-    //     if (!error) {
-    //       var object = JSON.parse(result.content);
-    //       restaurant.imageUrl = object.channel.item[0]?  object.channel.item[0].imageUrl: '';
-    //       restaurant.location.longitude = object.channel.item[0]? object.channel.item[0].longitude: '';
-    //       restaurant.location.latitude = object.channel.item[0]? object.channel.item[0].latitude: '';
+    HTTP.call('GET', requestURI, 
+        { params: { apikey: 'f836162338ea24d0e221975199009a2d', query : query } }
+        , function(error, result) {
+        if (!error) {
+          console.log(result.content);
+          var object = JSON.parse(result.content);
 
-    //     } else {
-    //       console.log(error);
-    //     }
-    //     Posts.insert(restaurant);
-    //   }
-    // );
-    Posts.insert(restaurant);
+          if( object.channel.item ) { 
+            restaurant.imageUrl = object.channel.item[0]?  object.channel.item[0].imageUrl: '';
+            restaurant.location.longitude = object.channel.item[0]? object.channel.item[0].longitude: '';
+            restaurant.location.latitude = object.channel.item[0]? object.channel.item[0].latitude: '';
+          }
+        } else {
+          console.log(error);
+        }
+        Posts.insert(restaurant);
+      }
+    );
   }
 }
 //test
