f9a8fd2e64edae8eaa1b2dc9d3b10bec70e1a47f
youngmin kim <youngmin@youngminui-MacBook-Pro.local>
2015-07-13 13:56:51 +0900

    moment handle

--- client/templates/comments/comment_item.js
+++ client/templates/comments/comment_item.js
@@ -1,7 +1,7 @@
 Template.commentItem.helpers({
   submittedText: function() {
   	if( moment().isSame(moment(this.submitted), 'day')) {
-			return moment(this.submittedText).startOf('hour').fromNow();
+  		return moment(this.submitted).format('YYYY-MM-DD mm-ss');	
   	} else {
   		return moment(this.submitted).format('YYYY-MM-DD mm-ss');	
   	}
