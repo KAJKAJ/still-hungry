4591c2f217faaee71d80d5ea41008493f2b4a70a
youngmin kim <youngmin@youngminui-MacBook-Pro.local>
2015-07-13 18:26:14 +0900

    tab for comments

--- client/templates/posts/post_page.js
+++ client/templates/posts/post_page.js
@@ -1,14 +1,27 @@
-Template.postPage.helpers({
+// Template.postPage.helpers({
+//   comments: function() {
+//     return Comments.find({postId: this._id});
+//   }
+// });
+
+Template.mapNComments.events({
+  "click a[data-tab='first']:not(.active)": function(e) {
+    e.preventDefault();
+ 	 	$("[data-tab='second']").removeClass('active');
+    $("[data-tab='first']").addClass('active');
+    e.target.classList.add('active');
+  },
+  "click a[data-tab='second']:not(.active)": function(e) {
+ 	 	e.preventDefault();
+ 	 	$("[data-tab='first']").removeClass('active');
+  	$("[data-tab='second']").addClass('active');
+  	e.target.classList.add('active');
+  }
+});
+
+Template.mapNComments.helpers({
   comments: function() {
     return Comments.find({postId: this._id});
   }
 });
 
-
-Template.mapNComments.events({
-  'click .ui.tabular.menu>.item:not(.active)': function(e) {
-    e.preventDefault();
-    $('.ui.tabular.menu>.item.active').removeClass('active');
-		e.target.classList.add('active');
-  }
-});
